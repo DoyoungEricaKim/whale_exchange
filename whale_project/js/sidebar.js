7528dee0a6444cea18280ecb68b34b629fd58420
ggy0151 <45171065+ggy0151@users.noreply.github.com>
2018-12-03 20:52:17 +0900

    changed 2 sidebar.js
    sidebar.js version2

--- whale_project/js/sidebar.js
+++ whale_project/js/sidebar.js
@@ -1,27 +1,25 @@
+(function(){
 
-
-function initialize(){
-
-  country = $('#selectCur').val()
-  wantRate = $('#wantKRW').val()
-  period = $('pdate').val()
-  term = $('alarmterm').val()
+  var country = $("#selectCur>[name=category]>option:selected").val();
+  var wantRate = $("#wantKRW").val();
+  var period = $("#pdate").val();
+  var term = $("#alarmterm").find("[name=term1]:checked").val();
+  var form = document.querySelector('form');
 
   var id = reservlist.length + 1;
   var info = [country, wantRate, period, term];
-  var jsonfile = JSON.stringify(info);
+//  var jsonfile = JSON.stringify(info);
   var jsonverb =  {};
   jsonverb[id] = info;
-
-  whale.storage.sync.set(jsonverb, function(){
-    alert('success');
+form.addEventListener('submit', function(evt){
+  var value = jsonverb;
+  evt.preventDefault();
+  whale.storage.sync.set("jsonverb", function() {
+    console.log('Value is set to ' + value);
   });
-
-  whale.storage.sync.set({ 'id' : info}, function() {
-            console.log('Value is set to ' + info);
-  });
+});
 
   whale.storage.sync.get(['id'], function(result) {
     console.log('Value currently is ' + result.id);
-  });
-}
+  });  
+})();
