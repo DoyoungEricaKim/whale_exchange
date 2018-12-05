e1014c160eb36907904698a7ffd3ed1063bf6117
ggy0151 <45171065+ggy0151@users.noreply.github.com>
2018-12-05 22:16:29 +0900

    change exRate.js
    change exRate.js function by using require-promise

--- whale_project/background/exRate.js
+++ whale_project/background/exRate.js
@@ -5,40 +5,74 @@ $('#checkb').on('click', (event) => {
   window.location.href = "check.html"
 })
 */
-const express = require('express');
-const cheerio = require('cheerio');
 const request = require('request');
+const cheerio = require('cheerio');
+const rp = require('request-promise');
+let url = "https://finance.naver.com/marketindex/exchangeList.nhn";
 
-find = function(callback){
-  var result = [];
-  request('https://finance.naver.com/marketindex/exchangeList.nhn', (error, response, html) => {
-//    let result = [];
-    if(!error && response.statusCode == 200) {
-      const $ = cheerio.load(html);
-      //const value = $('.tit').find('.sale');
-      const exchange = $('.tit').next();
+/*
+module.exports.find = function(){
+  return new Promise(function(resolve, reject){
+    let url = "https://finance.naver.com/marketindex/exchangeList.nhn";
+    var result = [];
+    request({
+      url, json:true}, function(error, response, body){
+  //    let result = [];
+      if(!error && response.statusCode == 200) {
+        const $ = cheerio.load(body);
+        //const value = $('.tit').find('.sale');
+        const exchange = $('.tit').next();
 
-      for(let i = 0; i < 4; i++) {
-        let a = $(exchange[i]).text();
-        result.push(a);
+        for(let i = 0; i < 4; i++) {
+          let a = $(exchange[i]).text();
+          result.push(a);
+        }
       }
-    }
-    console.log("request:" + result);
+      console.log("request:" + result);
+      resolve(result);
+    });
+    console.log(result);
   });
-  console.log(result);
-  callback(result);
+
+};
+
+var result1 = search();
+console.log("result1: "+ result1);
+
+*/
+
+search(url, true).then(function(val){
+  console.log("result in search: "+val);
+}, function(err){
+  console.error("%s; %s", err.message, url);
+  console.log("%j", err.res.statusCode);
+});
+
+function search(url, json){
+  json = json || false;
+  return new Promise(function(resolve, reject){
+    var result = [];
+    request({url:url, json:json}, function(err, res, body){
+      if(!err && res.statusCode == 200) {
+        const $ = cheerio.load(body);
+        //const value = $('.tit').find('.sale');
+        const exchange = $('.tit').next();
+
+        for(let i = 0; i < 4; i++) {
+          let a = $(exchange[i]).text();
+          result.push(a);
+        }
+      }
+      console.log("first result:"+result);
+      resolve(result);
+    });
+  });
 }
 
 /*
 find = function(data, callback){
   var final_data =
 }
-
-var result1 = findRate();
-console.log(result1);
-
-
-/*
 module.exports.passfunc(resultRate) = function passfunc(resultRate){
   return callback(resultRate)
 }
---
+++ whale_project/background/getRate.js
@@ -0,0 +1,8 @@
+const cheerio = require('cheerio');
+const request = require('request');
+const promise = require('promise');
+
+var resultModule = require("exRate.js");
+console.log(resultModule);
+resultModule.find().then(function(result));
+console.log(resultModule);
