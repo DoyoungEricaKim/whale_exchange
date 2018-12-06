78504996822cc726feaaca2eff6a3d99d10fbd9f
ggy0151 <45171065+ggy0151@users.noreply.github.com>
2018-12-06 13:33:04 +0900

    update index.html & main.css
    updated main.css & index.html

--- whale_project/background/exRate.js
+++ whale_project/background/exRate.js
@@ -10,43 +10,13 @@ const cheerio = require('cheerio');
 const rp = require('request-promise');
 let url = "https://finance.naver.com/marketindex/exchangeList.nhn";
 
-/*
-module.exports.find = function(){
-  return new Promise(function(resolve, reject){
-    let url = "https://finance.naver.com/marketindex/exchangeList.nhn";
-    var result = [];
-    request({
-      url, json:true}, function(error, response, body){
-  //    let result = [];
-      if(!error && response.statusCode == 200) {
-        const $ = cheerio.load(body);
-        //const value = $('.tit').find('.sale');
-        const exchange = $('.tit').next();
-
-        for(let i = 0; i < 4; i++) {
-          let a = $(exchange[i]).text();
-          result.push(a);
-        }
-      }
-      console.log("request:" + result);
-      resolve(result);
-    });
-    console.log(result);
-  });
-
-};
-
-var result1 = search();
-console.log("result1: "+ result1);
-
-*/
 
 search(url, true).then(function(val){
   console.log("result in search: "+val);
-}, function(err){
+}), function(err){
   console.error("%s; %s", err.message, url);
   console.log("%j", err.res.statusCode);
-});
+};
 
 function search(url, json){
   json = json || false;
@@ -55,7 +25,6 @@ function search(url, json){
     request({url:url, json:json}, function(err, res, body){
       if(!err && res.statusCode == 200) {
         const $ = cheerio.load(body);
-        //const value = $('.tit').find('.sale');
         const exchange = $('.tit').next();
 
         for(let i = 0; i < 4; i++) {
@@ -69,11 +38,22 @@ function search(url, json){
   });
 }
 
+
 /*
-find = function(data, callback){
-  var final_data =
-}
-module.exports.passfunc(resultRate) = function passfunc(resultRate){
-  return callback(resultRate)
-}
+//fetch
+const getDataFetch = url => (
+  fetch(url).then(res => res.json())
+);
+
+var res = data => {
+  for(let key in data){
+    if(data.hasOwnProperty(key))
+      console.log(`${key}: ${data[key]}`);
+  }
+};
+
+var promise4 = fetch(url).then(res=>res.json()
+  res.json()
+
+);
 */
